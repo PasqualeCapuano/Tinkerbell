844add8ba2585a28373b53189957bda6daeeda69
Pasquale Capuano <p.capuano.ita@be-tse.it>
2023-11-20 20:16:33 +0100

    gave some content to the landing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import {LoginComponent} from "./components/login/login.component";
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: 'login', component: LoginComponent },
 ];
