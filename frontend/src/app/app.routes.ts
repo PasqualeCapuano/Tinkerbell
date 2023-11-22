19e532e87f47a4c2cd82cfcfa6ecae404271cf35
Pasquale Capuano <p.capuano.ita@be-tse.it>
2023-11-22 09:25:01 +0100

    sidebar elements and routing new components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {LandingPageComponent} from "./components/landing-page/landing-page.component";
 import {LoginComponent} from "./components/login/login.component";
+import { SidebarComponent} from "./components/sidebar/sidebar.component";
+import { HomeComponent} from "./components/home/home.component";
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'home', component: HomeComponent },
 ];
